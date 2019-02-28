25cc58c9ab8aa515ae9781ca8ceb0d295c81414b
01099473080 <01099473080>
2019-02-28 14:58:22 +0900

    2019.02.28 14:58

--- WebContent/resources/js/oop.js
+++ WebContent/resources/js/oop.js
@@ -4,28 +4,54 @@ function People(name,age,gender,job){
 	this.gender= gender;
 	this.job = job;
 }
-function Cust(name,age,gender,job,grade,custNo){
-	People.call(this,name,age,gender,job);
+People.prototype.setName = name=>{this.name = name;};
+People.prototype.setAge = age=>{this.age = age;};
+People.prototype.setGender = gender=>{this.gender = gender;};
+People.prototype.setJob = job=>{this.job = job;};
+People.prototype.getName = ()=>{return this.name;};
+People.prototype.getAge = ()=>{return this.age;};
+People.prototype.getGender = ()=>{return this.gender;};
+People.prototype.getJob= ()=>{return this.job;};
+
+
+function Customer(grade,custNo){
+	People.apply(this,arguments);
 	this.grade = grade;
 	this.custNo = custNo;
 }
+Customer.prototype = new People();
 
-var test1= (()=>{
-	let main = ()=>{
-		let cust = new Cust('홍길동','25세','남','개발자','3급','123');
-		alert('고객이름 : \n' + cust.name 
-				+ '고객나이 :\n' + cust.age 
-				+ '고객성별 :\n' + cust.gender 
-				+ '고객직업 :\n' + cust.job 
-				+ '고객등급 :\n' + cust.grade 
-				+'고객번호 :\n' + cust.custNo);
-	}
-	return {main: main};
-})();
+Customer.prototype.setName = name=>{
+	//this.name='[닉네임]'+name 이건 자바 문법
+	Object.getPrototypeOf(Customer.prototype).setName('[닉네임]'+name);
+};
+Customer.prototype.setGrade = grade=>{this.grade=grade;};
+Customer.prototype.setCustNo = custNo=>{this.custNo=custNo;};
+Customer.prototype.getGrade= ()=>{return this.grade;};
+Customer.prototype.getCustNo= ()=>{return this.custNo;};
 
-/*alert('고객정보 : 홍길동,25세,남,개발자,3급,123')
+var test2= {
+		main: ()=>{
+		let cust = new Customer();
+		cust.setName('홍길동');
+		cust.setAge('25세'); 
+		cust.setGender('남'); 
+		cust.setJob('개발자');
+		cust.setGrade('3급');
+		cust.setCustNo('123');
+		alert('고객정보 :'+cust.getName()+'\n'
+				+cust.getAge()+'\n'
+				+cust.getGender()+'\n'
+				+cust.getJob()+'\n'
+				+cust.getGrade()+'\n'
+				+cust.getCustNo());
+		}};
+		/*alert('고객정보 : 홍길동,25세,남,개발자,3급,123')
+
+
+
 */
-function Product(name,price) {
+/*function Product(name,price) {
 	this.name = name;
 	this.price = price;
 }
@@ -41,11 +67,6 @@ var test = (()=>{
 	}
 	return {main : main};
 })();
-
-
-
-
-
 
 var inherit = (()=>{
 	return{
@@ -118,3 +139,4 @@ var person = (()=>{
 		toString : toString
 	}
 })();
+*/
